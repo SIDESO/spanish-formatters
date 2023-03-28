ef48993e1d170afb1232c47c7c934f388bf2a9f1
Sebastian Diaz <sdiazarrieta0627@gmail.com>
2023-03-28 16:40:45 -0500

    Add month methods and fixed toDocument method

--- src/Date.php
+++ src/Date.php
@@ -6,9 +6,49 @@ use Carbon\Carbon;
 
 class Date
 {
-    public static function toDocument(Carbon $date)
+    /**
+     * Get months in spanish.
+     * 
+     * @param bool $short Return short names
+     * 
+     * @return array
+     */
+    public static function getMonths(bool $short = false): array
     {
-        $months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-        return 'a los '.$date->day.' días del mes de '.$months[$date->month - 1].' de '.$date->year;
+        if ($short) {
+            return ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
+        }
+
+        return ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+    }
+
+    /**
+     * Get month name in spanish.
+     * 
+     * @param Carbon $date Date to get month name
+     * @param bool $short Return short name
+     * 
+     * @return string
+     */
+    public static function getMonth(Carbon $date, bool $short = false): string
+    {
+        return self::getMonths($short)[$date->month - 1];
+    }
+
+    /**
+     * Get date in spanish format for documents.
+     * 
+     * @param Carbon $date Date to format
+     * @param bool $longText Return long text
+     * 
+     * @return string
+     */
+    public static function toDocument(Carbon $date, bool $longText = true): string
+    {
+        if(! $longText){
+            return $date->day.' de '.self::getMonth($date).' del '.$date->year;
+        }
+
+        return 'a los '.$date->day.' días del mes de '.self::getMonth($date).' del '.$date->year;
     }
 }
