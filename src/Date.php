3bf9e40090a903f5c5236cb701b4470065dd5036
Sebastian Diaz <sdiazarrieta0627@gmail.com>
2024-03-27 19:11:17 -0500

    toLocalized format in dates

--- src/Date.php
+++ src/Date.php
@@ -7,6 +7,35 @@ use Carbon\Carbon;
 class Date
 {
     /**
+     * Get days in spanish.
+     * 
+     * @param bool $short Return short names
+     * 
+     * @return array
+     */
+    public static function getDays(bool $short = false): array
+    {
+        if ($short) {
+            return ['Lun','Mar','Mié','Jue','Vie','Sáb','Dom'];
+        }
+
+        return ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+    }
+
+    /**
+     * Get day name in spanish.
+     * 
+     * @param Carbon $date Date to get day name
+     * @param bool $short Return short name
+     * 
+     * @return string
+     */
+    public static function getDay(Carbon $date, bool $short = false): string
+    {
+        return self::getDays($short)[$date->dayOfWeek - 1];
+    }
+
+    /**
      * Get months in spanish.
      * 
      * @param bool $short Return short names
@@ -50,5 +79,28 @@ class Date
         }
 
         return 'a los '.$date->day.' días del mes de '.self::getMonth($date).' del '.$date->year;
+    }
+
+    /**
+     * Get date in spanish format for localized.
+     * 
+     * @param Carbon $date Date to format
+     * @param bool $longText Return long text
+     * 
+     * @return string
+     */
+    public static function toLocalized(Carbon $date, string $format = '%a %d de %b de %Y', bool $longText = true): string
+    {
+        $day = $date->day;
+        $dayOfWeek = self::getDay($date, !$longText);
+        $month = self::getMonth($date, !$longText);
+        $year = $date->year;
+
+        $dateFormatted = str_replace('%a', $dayOfWeek, $format);
+        $dateFormatted = str_replace('%d', $day, $dateFormatted);
+        $dateFormatted = str_replace('%b', $month, $dateFormatted);
+        $dateFormatted = str_replace('%Y', $year, $dateFormatted);
+
+        return $dateFormatted;
     }
 }
