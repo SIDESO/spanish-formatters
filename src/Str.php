38aec075f55c4ba7ba52bec438ab7bdda5f49e05
Sebastian Diaz <sdiazarrieta0627@gmail.com>
2024-10-31 12:18:32 -0500

    Add splitFullName in Str Class

--- src/Str.php
+++ src/Str.php
@@ -2,6 +2,8 @@
 
 namespace Sideso\SpanishFormatters;
 
+use Illuminate\Support\Str as BaseStr;
+
 class Str
 {
     /**
@@ -14,7 +16,7 @@ class Str
      * 
      * @return string
      */
-    public static function toNormalize($string, bool $toUpper = true, bool $applyPregReplace = true, string $pregReplacePattern = '/[^a-zA-Z0-9]/') 
+    public static function toNormalize($string, bool $toUpper = true, bool $applyPregReplace = true, string $pregReplacePattern = '/[^a-zA-Z0-9]/')
     {
         // Replace accented characters
         $string = str_replace(
@@ -22,17 +24,98 @@ class Str
             ['a', 'e', 'i', 'o', 'u', 'n', 'u', 'A', 'E', 'I', 'O', 'U', 'N', 'U'],
             $string
         );
-    
+
         if ($applyPregReplace) {
             // Delete special characters
             $string = preg_replace($pregReplacePattern, '', $string);
         }
-    
+
         if ($toUpper) {
             // Convertir a mayúsculas
             $string = strtoupper($string);
         }
-    
+
         return $string;
+    }
+
+    /**
+     * Split a full name into first name, second name, last name and mother's last name
+     * 
+     * @param string $full_name Full name to split
+     * 
+     * @return array
+     */
+    public static function splitFullName(string $full_name)
+    {
+        /* Convertir a mayúsculas */
+        $full_name = BaseStr::upper($full_name);
+
+        /* Separar el nombre completo por espacios */
+        $tokens = BaseStr::of($full_name)->trim()->explode(' ');
+
+        /* Arreglo donde se guardan las "palabras" del nombre */
+        $names = array();
+
+        /* Palabras para apellidos (y nombres) compuestos */
+        $special_tokens = array('DA', 'DE', 'DEL', 'LA', 'LAS', 'LOS', 'MAC', 'MC', 'VAN', 'VON', 'Y', 'I', 'SAN', 'SANTA');
+
+        $prev = "";
+        foreach ($tokens as $token) {
+            $_token = $token;
+            if (in_array($_token, $special_tokens)) {
+                $prev .= "$token ";
+            } else {
+                $names[] = $prev . $token;
+                $prev = "";
+            }
+        }
+
+        // Eliminar espacios en blanco
+        $names = collect($names)->map(fn($name) => trim($name))->filter()->values()->toArray();
+
+        $last_name = '';
+        $second_last_name = '';
+        $first_name = '';
+        $middle_name = '';
+
+        $num_names = count($names);
+
+        switch ($num_names) {
+            case 0:
+                break;
+            case 1:
+                $first_name = $names[0];
+                break;
+            case 2:
+                $first_name = $names[0];
+                $last_name = $names[1];
+                break;
+            case 3:
+                $first_name = $names[0];
+                $last_name = $names[1];
+                $second_last_name = $names[2];
+                break;
+            case 4:
+                $first_name = $names[0];
+                $middle_name = $names[1];
+                $last_name = $names[2];
+                $second_last_name = $names[3];
+                break;
+            default:
+                $first_name = "{$names[0]} {$names[1]}";
+                $middle_name = $names[2];
+                $last_name = $names[3];
+
+                unset($names[0], $names[1], $names[2], $names[3]);
+                $second_last_name = implode(' ', $names);
+                break;
+        }
+
+        return [
+            'last_name' => $last_name,
+            'second_last_name' => $second_last_name,
+            'first_name' => $first_name,
+            'middle_name' => $middle_name,
+        ];
     }
 }
